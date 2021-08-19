1fa5d12bb198be22861b0219f1bc308cb0cf6461
Flávio Santos <flavioraph@live.com>
2021-08-18 23:31:55 -0300

    nova correcao

--- katas5.js
+++ katas5.js
@@ -13,8 +13,9 @@ function testeReverseString2() {
 
 function reverseString(str) {
     return str.split('').reverse().join('');
-
 }
+console.log(testeReverseString1())
+console.log(testeReverseString2())
 
 
 //2
@@ -33,6 +34,9 @@ function testReverseSentence2() {
 function reverseSentence(str) {
     return str.split(' ').reverse().join(' ');
 }
+console.log(testReverseSentence1())
+console.log(testReverseSentence2())
+
 //3
 function testMinimumValue1() {
     let result = minimumValue([5, 4, 3, 2, 1]);
@@ -50,7 +54,9 @@ function minimumValue(arr) {
     let order = arr.sort((a, b) => a - b)
     return order[0]
 }
-//4
+console.log(testMinimumValue1())
+console.log(testMinimumValue2())
+    //4
 function testMaximumValue1() {
     let result = maximumValue([5, 4, 3, 2, 1]);
     let expected = 5;
@@ -67,67 +73,91 @@ function maximumValue(arr) {
     let order = arr.sort((a, b) => b - a)
     return order[0]
 }
-//5
+console.log(testMaximumValue1())
+console.log(testMaximumValue2())
+    //5
 function testCalculateRemainder1() {
-    let result = calculateRemainder(3 / 2);
+    let result = calculateRemainder(3, 2);
     let expected = 1;
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
 
 function testCalculateRemainder2() {
-    let result = calculateRemainder(5 / 3);
+    let result = calculateRemainder(5, 3);
     let expected = 2;
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
 
 function calculateRemainder(a, b) {
-    return a % b
+    let resto = a % b
+    return resto;
 }
-//6
+console.log(testCalculateRemainder1())
+console.log(testCalculateRemainder2())
+    //6
 function testDistinctValues1() {
     let result = distinctValues([1, 3, 5, 3, 7, 3, 1, 1, 5]);
-    let expected = [1, 3, 5, 7];
+    let expected = "1, 3, 5, 7";
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
 
 function testDistinctValues2() {
     let result = distinctValues([1, 1, 2, 2, 3, 3, 4, 4]);
-    let expected = [1, 2, 3, 4];
-    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+    let expected = "1, 2, 3, 4";
+    console.assert(result === expected, `esperado: ${expected} , obtido: ${result}`)
 }
 
 function distinctValues(arr) {
+    const distinct = [...new Set(arr)];
+    return distinct.sort((a, b) => a - b).join(', ')
 
 }
-//7
-function testDistinctValues1() {
-    let result = distinctValues([1, 3, 5, 3, 7, 3, 1, 1, 5]);
-    let expected = [1, 3, 5, 7];
+console.log(testDistinctValues1())
+console.log(testDistinctValues2())
+
+function testCountValues1() {
+    let result = countValues([1, 3, 5, 3, 7, 3, 1, 1, 5]);
+    let expected = '1(3) 3(3) 5(2) 7(1)';
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
 
-function testDistinctValues2() {
-    let result = distinctValues([1, 1, 2, 2, 3, 3, 4, 4]);
-    let expected = [1, 2, 3, 4];
+function testCountValues2() {
+    let result = countValues([1, 1, 2, 2, 3, 3, 4, 4]);
+    let expected = '1(2) 2(2) 3(2) 4(2)';
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
 
-function distinctValues(arr) {
-
+function countValues(arr) {
+    let cont = 0;
+    let str = ''
+    const distinct = [...new Set(arr)];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; distinct.length; j++) {
+            if (distinct[j] === arr[i]) {
+                cont++;
+                str += `arr[i]` + `${cont}`
+            }
+        }
+    }
 }
-//8
-function testDistinctValues1() {
-    let result = distinctValues([1, 3, 5, 3, 7, 3, 1, 1, 5]);
-    let expected = [1, 3, 5, 7];
+console.log(testCountValues1())
+    //8
+function testEvaluateExpression1() {
+    let result = distinctValues("a + b + c - d", { a: 1, b: 7, c: 3, d: 14 });
+    let expected = -3;
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
 
-function testDistinctValues2() {
-    let result = distinctValues([1, 1, 2, 2, 3, 3, 4, 4]);
-    let expected = [1, 2, 3, 4];
+function testEvaluateExpression2() {
+    let result = distinctValues("a + b + c - d", { a: 1, b: 2, c: 3, d: 4 });
+    let expected = 2;
     console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
 
-function distinctValues(arr) {
-
+function evaluateExpression(str, obj) {
+    let letter = str.match(/[a-z]/gi);
+    letter.forEach(element => {
+        console.log(obj.element)
+    });
+    console.log(letter)
 }
